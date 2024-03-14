ff4462b55b9fc4bc22c4f360af51a2a1ef6dfe78
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-03-14 15:42:31 +0800

    added conversion and post functions

--- post-data.py
+++ post-data.py
@@ -1,9 +1,12 @@
 import requests
-
-# Sensor values
-# temp = '33.2' #get data from rpi and dht11
-# humid = '2.0' #get data from rpi and dht11
-# nh3 = '20' #get data from rpi and mq137
+# import Adafruit_DHT
+# sensor = Adafruit_DHT.DHT11
+# pin = 27
+# humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+# temperature = temperature *(9/5) + 32
+# Convert values to float
+# temperature = float(temperature)
+# humidity = float(humidity)
 
 new_data = {
     "userID": 1,
@@ -11,6 +14,16 @@ new_data = {
     "title": "Making a POST request",
     "body": "This is the data we created."
 }
+api_url = "https://jsonplaceholder.typicode.com/posts"
+response = requests.post(api_url,json=new_data)
+response_dict = response.json()
+print(response_dict)
+
+
+# Sensor values
+# temp = temperature #get data from rpi and dht11
+# humid = humidity #get data from rpi and dht11
+# nh3 = '20' #get data from rpi and mq137
 
 # temp_value = {
 #     "value": temp,
@@ -22,8 +35,19 @@ new_data = {
 #     "value": nh3,
 # }
 
-# POST dht data: temperature and humidity
-api_url = "https://jsonplaceholder.typicode.com/posts"
-response = requests.post(api_url,json=new_data)
-response_dict = response.json()
-print(response_dict)
+# api_url = "https://jsonplaceholder.typicode.com/posts"
+
+# # POST dht data: temperature
+# response = requests.post(api_url,json=temp_value)
+# res = response.json()
+# print(res)
+
+# # POST dht data: humidity
+# response = requests.post(api_url,json=humid_value)
+# res = response.json()
+# print(res)
+
+# # POST dht data: ammonia
+# response = requests.post(api_url,json=nh3_value)
+# res = response.json()
+# print(res)
