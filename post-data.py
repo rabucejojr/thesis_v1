f0af23a51c357e439fd8e61a878b987d64fa8476
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-03-14 14:04:58 +0800

    ghghj

--- post-data.py
+++ post-data.py
@@ -1,10 +1,11 @@
 import requests
 
 # Sensor values
-temp = '33.2'
-humid = '2.0'
-nh3 = '20'
+temp = '33.2' #get data from rpi and dht11
+humid = '2.0' #get data from rpi and dht11
+nh3 = '20' #get data from rpi and mq137
+
 # POST dht data: temperature and humidity
-response = requests.get("https://dummyjson.com/posts/5")
+response = requests.post("https://dummyjson.com/posts/5")
 response_dict = response.json()
 print(response_dict)
