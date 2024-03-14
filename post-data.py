519ab5d6248e6c12dfc3268fc909a172449854c5
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-03-14 14:21:08 +0800

    add dummy data for testing ang code comments

--- post-data.py
+++ post-data.py
@@ -1,11 +1,29 @@
 import requests
 
 # Sensor values
-temp = '33.2' #get data from rpi and dht11
-humid = '2.0' #get data from rpi and dht11
-nh3 = '20' #get data from rpi and mq137
+# temp = '33.2' #get data from rpi and dht11
+# humid = '2.0' #get data from rpi and dht11
+# nh3 = '20' #get data from rpi and mq137
+
+new_data = {
+    "userID": 1,
+    "id": 1,
+    "title": "Making a POST request",
+    "body": "This is the data we created."
+}
+
+# temp_value = {
+#     "value": temp,
+# }
+# humid_value = {
+#     "value": humid,
+# }
+# nh3_value = {
+#     "value": nh3,
+# }
 
 # POST dht data: temperature and humidity
-response = requests.post("https://dummyjson.com/posts/5")
+api_url = "https://jsonplaceholder.typicode.com/posts"
+response = requests.post(api_url,json=new_data)
 response_dict = response.json()
 print(response_dict)
