caef3708ba1dffb5872100143939ed2865f757c9
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-03-17 09:49:25 +0800

    ir code reference

--- ir.py
+++ ir.py
@@ -1,14 +1,29 @@
 import RPi.GPIO as GPIO
 import time
 
-ir_pin = 26
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(ir_pin, GPIO.IN)
+# Set GPIO mode and pin for IR sensor
+GPIO.setmode(GPIO.BOARD)
+IR_PIN = 11
+GPIO.setup(IR_PIN, GPIO.IN)
+
+# Initialize variables
+egg_count = 0
+debounce_delay = 0.3  # Debounce delay in seconds
+last_detection_time = time.time()
 
 try:
     while True:
-        ir_state=GPIO.input(ir_pin)
-        print('IR STATE:',ir_state)
-        time.sleep(0.5)
+        # Check if IR sensor is triggered
+        if GPIO.input(IR_PIN) == GPIO.LOW:
+            # Perform debounce
+            if time.time() - last_detection_time > debounce_delay:
+                egg_count += 1
+                print("Egg detected! Total eggs counted:", egg_count)
+                last_detection_time = time.time()
+        
+        # You can add additional logic here, such as saving the count to a file or database.
+
+        time.sleep(0.1)  # Sleep to reduce CPU usage
+
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    GPIO.cleanup()  # Clean up GPIO on CTRL+C exit
