1654db9d8f25082de3461909f5b4002e358a214b
rabucejojr <rogerhapay@gmail.com>
2024-03-24 10:11:40 +0800

    add relay module code

--- test-code/relay.py
+++ test-code/relay.py
@@ -1,12 +1,45 @@
+# getting the main GPIO libraly
 import RPi.GPIO as GPIO
-from time import sleep
+# getting the time libraly
+import time
 
-GPIO.setwarnings(False)
+# setting a current mode
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+#removing the warings 
+GPIO.setwarnings(False)
+#creating a list (array) with the number of GPIO's that we use 
+pins = [23,24] 
 
-while True:
-    GPIO.output(18, 1)
-    sleep(1)
-    GPIO.output(18, 0)
-    sleep(1)
+#setting the mode for all pins so all will be switched on 
+GPIO.setup(pins, GPIO.OUT)
+
+#for loop where pin = 23 and 24, this refers to gpio pin numbers, not pin sequence
+for pin in pins :
+	#setting the GPIO to HIGH or 1 or true
+	GPIO.output(pin,  GPIO.HIGH)
+	#wait 0,5 second
+	time.sleep(2)
+	#setting the GPIO to LOW or 0 or false
+	GPIO.output(pin,  GPIO.LOW)
+	#wait 0,5 second
+	time.sleep(2)
+
+	#Checking if the current relay is running and printing it 
+	if not GPIO.input(pin) : 
+		print("Pin "+str(pin)+" is working" )
+		
+
+#same but the difference is that  we have 
+#for loop where pin = 14 next 15,17,18
+# backwards
+for pin in reversed(pins) :
+	GPIO.output(pin,  GPIO.HIGH)
+	time.sleep(2)
+
+	GPIO.output(pin,  GPIO.LOW)
+	time.sleep(2)
+
+
+#cleaning all GPIO's 
+GPIO.cleanup()
+print ("Shutdown All relays")
