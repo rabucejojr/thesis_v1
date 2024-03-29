d3c941db3c9f42118f0028eab5de668911580de1
rabucejojr <rogerhapay@gmail.com>
2024-03-29 08:48:13 +0800

    update servo code using gpiozero library

--- test-code/servo.py
+++ test-code/servo.py
@@ -1,23 +1,10 @@
-import RPi.GPIO as GPIO
+from gpiozero import AngularServo
 import time
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+servo = AngularServo(17,min_angle=-180,max_angle=180)
 
-p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
-p.start(2.5)  # Initialization
-try:
-    while True:
-        p.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(10)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(12.5)
-        time.sleep(0.5)
-
-except KeyboardInterrupt:
-    p.stop()
-    GPIO.cleanup()
+while True:
+    servo.angle = -180
+    time.sleep(2)
+    servo.angle = 180
+    time.sleep(2)
