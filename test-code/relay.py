363845fe9fb1eb555b2598e52eacb2c7f6ee8157
rabucejojr <rogerhapay@gmail.com>
2024-03-29 12:44:40 +0800

    refine code for relay

--- test-code/relay.py
+++ test-code/relay.py
@@ -1,46 +1,24 @@
-# getting the main GPIO libraly
-import RPi.GPIO as GPIO
+from gpiozero import OutputDevice
+from time import sleep
 
-# getting the time libraly
-import time
+pin1 = 17
+pin2 = 22
 
-# setting a current mode
-GPIO.setmode(GPIO.BCM)
-# removing the warings
-GPIO.setwarnings(False)
-# creating a list (array) with the number of GPIO's that we use
-pins = [23, 24]
+relay1 = OutputDevice(pin1,active_high=False, initial_value=False)
+relay2 = OutputDevice(pin2,active_high=False, initial_value=False)
+try:
+    while True:
+        relay1.on()
+        sleep(2)
+        relay1.off()
+        sleep(2)
+        relay2.on()
+        sleep(2)
+        relay2.off()
+        sleep(2)
+except KeyboardInterrupt:
+    print("Exiting Program")
 
-# setting the mode for all pins so all will be switched on
-GPIO.setup(pins, GPIO.OUT)
-
-# for loop where pin = 23 and 24, this refers to gpio pin numbers, not pin sequence
-for pin in pins:
-    # setting the GPIO to HIGH or 1 or true
-    GPIO.output(pin, GPIO.HIGH)
-    # wait 0,5 second
-    time.sleep(2)
-    # setting the GPIO to LOW or 0 or false
-    GPIO.output(pin, GPIO.LOW)
-    # wait 0,5 second
-    time.sleep(2)
-
-    # Checking if the current relay is running and printing it
-    if not GPIO.input(pin):
-        print("Pin " + str(pin) + " is working")
-
-
-# same but the difference is that  we have
-# for loop where pin = 14 next 15,17,18
-# backwards
-for pin in reversed(pins):
-    GPIO.output(pin, GPIO.HIGH)
-    time.sleep(2)
-
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(2)
-
-
-# cleaning all GPIO's
-GPIO.cleanup()
-print("Shutdown All relays")
+finally:
+    relay1.close()
+    relay2.close()
