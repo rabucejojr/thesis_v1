4b74e17d994d7c78c3d281d4b5eba8ffe0eafd70
rabucejojr <rogerhapay@gmail.com>
2024-04-04 22:13:09 +0800

    code block added

--- auto-feeder.py
+++ auto-feeder.py
@@ -1,3 +1,30 @@
 # this will be executed thru cron, with schedules 7AM and 4PM, two servos for feeds and water
 # crontab for 7AM activation: 0 7 * * * /home/admin/Desktop/thesis_v1/auto-feeder.py
 # crontab for 4PM activation: 0 16 * * * /home/admin/Desktop/thesis_v1/auto-feeder.py
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+servo1 = 29
+servo2 = 31
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False) 
+GPIO.setup(servo1, GPIO.OUT)
+GPIO.setup(servo2, GPIO.OUT)
+p1 = GPIO.PWM(servo1, 50) 
+p2 = GPIO.PWM(servo2, 50)# GPIO 17 for PWM with 50Hz
+p1.start(0)
+p2.start(0)# Initialization
+try:
+    while True:
+        p1.ChangeDutyCycle(2.5)
+        p2.ChangeDutyCycle(2.5)
+        sleep(10)
+        p1.ChangeDutyCycle(12.5)
+        p2.ChangeDutyCycle(12.5)
+        sleep(10)
+
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
+
--- test-code/servo.py
+++ test-code/servo.py
@@ -4,25 +4,21 @@ from time import sleep
 servo1 = 29
 servo2 = 31
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False) 
 GPIO.setup(servo1, GPIO.OUT)
 GPIO.setup(servo2, GPIO.OUT)
 p1 = GPIO.PWM(servo1, 50) 
 p2 = GPIO.PWM(servo2, 50)# GPIO 17 for PWM with 50Hz
-p.start(2.5)  # Initialization
+p1.start(0)
+p2.start(0)# Initialization
 try:
     while True:
-        p1.ChangeDutyCycle(5)
-        p2.ChangeDutyCycle(5)
-        sleep(0.5)
-        p1.ChangeDutyCycle(7.5)
-        p2.ChangeDutyCycle(7.5)
-        sleep(0.5)
-        p1.ChangeDutyCycle(10)
-        p2.ChangeDutyCycle(10)
-        sleep(0.5)
+        p1.ChangeDutyCycle(2.5)
+        p2.ChangeDutyCycle(2.5)
+        sleep(4)
         p1.ChangeDutyCycle(12.5)
         p2.ChangeDutyCycle(12.5)
-        sleep(0.5)
+        sleep(4)
 
 except KeyboardInterrupt:
     p.stop()
