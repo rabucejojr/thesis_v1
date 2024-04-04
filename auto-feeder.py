d35d301e9a57b0731b377d6ff5ef33e04c30c2a6
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-04-04 17:25:42 +0800

    added code comment guide for crontab

--- auto-feeder.py
+++ auto-feeder.py
@@ -1,2 +1,3 @@
 # this will be executed thru cron, with schedules 7AM and 4PM, two servos for feeds and water
-
+# crontab for 7AM activation: 0 7 * * * /home/admin/Desktop/thesis_v1/auto-feeder.py
+# crontab for 4PM activation: 0 16 * * * /home/admin/Desktop/thesis_v1/auto-feeder.py
