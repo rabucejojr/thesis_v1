4e19b72750a6b6e42734070ced685a8ea36ae77f
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-04-12 20:32:03 +0800

    added code line for cron command

--- auto-feeder.py
+++ auto-feeder.py
@@ -1,6 +1,8 @@
 # this will be executed thru cron, with schedules 7AM and 4PM, two servos for feeds and water
 # crontab for 7AM activation: 0 7 * * * /home/admin/Desktop/thesis_v1/auto-feeder.py
 # crontab for 4PM activation: 0 16 * * * /home/admin/Desktop/thesis_v1/auto-feeder.py
+# this code executes main controller to read and submit data to api
+# @reboot python3 /home/admin/Desktop/thesis_v1/piggery_hardware_controller.py
 
 import RPi.GPIO as GPIO
 from time import sleep
